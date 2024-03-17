2c797ce8adbcf8a5e22f3b262f6fc09bbe5b0154
Liu <zehao100@qq.com>
2024-03-17 21:27:18 +0800

    Update media query breakpoint in global styles

--- src/utils/screen.ts
+++ src/utils/screen.ts
@@ -1,6 +1,8 @@
 // mobile 设备宽度
 // https://github.com/ant-design/ant-design/blob/master/components/theme/util/alias.ts#L28
 export const screenSM = 576;
+// https://github.com/ant-design/ant-design/blob/master/components/theme/util/alias.ts#L29
+export const screenMD = 768;
 
 /**
  * check mobile device in browser
@@ -10,5 +12,5 @@ export const isMobileScreen = () => {
     return false;
   }
 
-  return window.innerWidth <= screenSM;
+  return window.innerWidth <= screenMD;
 };
